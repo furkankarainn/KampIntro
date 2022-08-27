15a4581af29cbf0f4bf24f1491846cf8be1fbcaf
Furkan Karain <furkan.karain@antsoft.com.tr>
2022-08-27 16:24:23 +0300

    Test

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -13,6 +13,8 @@ namespace DegerVeReferansTipler
             sayi2 = 65;
             //sayi1 ?? => (30)
 
+
+
             int[] sayilar1 = new int[] { 10, 20, 30 };
             int[] sayilar2 = new int[] { 100, 200, 300 };
 
