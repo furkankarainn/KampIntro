de43625488ba900b32431089ad7aadbc1b12f394
Furkan Karain <furkan.karain@antsoft.com.tr>
2022-08-29 19:31:01 +0300

    Generics Update

--- MyDictionary/Program.cs
+++ MyDictionary/Program.cs
@@ -6,7 +6,9 @@ namespace MyDictionary
     {
         static void Main(string[] args)
         {
-            MyDictionary<int> myDictionary = new MyDictionary<int>();
+            MyDictionaries<int> myDictionary = new MyDictionaries<int>();
+            myDictionary.Add(112);
+            myDictionary.Add(112);
             myDictionary.Add(112);
             myDictionary.Add(112);
             myDictionary.Add(112);
@@ -14,33 +16,5 @@ namespace MyDictionary
 
             Console.ReadLine();
         }
-    }
-
-    class MyDictionary<T>
-    {
-        T[] _array;
-        public MyDictionary()
-        {
-            _array = new T[0];
-        }
-        
-        public void Add(T item)
-        {
-            T[] _tempArray = _array;
-
-            _array = new T[_array.Length + 1];
-            for (int i = 0; i < _tempArray.Length; i++)
-            {
-                _array[i] = _tempArray[i];
-            }
-            _array[_tempArray.Length] = item;
-        }
-
-        public int Count
-        {
-            get { return _array.Length; }
-        }
-
-
     }
 }
